e2a876a6dcdcfbf8b0e6a33b67e2988719c7cf5b
Titus <Titus>
2016-08-27 01:11:56 -0400

    late night sleepy

--- ludum-dare/game/core.cljs
+++ ludum-dare/game/core.cljs
@@ -1,5 +1,5 @@
 (ns game.core
-  (:require [malandragem.core :as rl]))
+  (:require [malandragem.core :as rl :refer [px]]))
 
 (enable-console-print!)
 
@@ -21,11 +21,35 @@
   (rl/data
    :levels levels
    :state {:level 0}
+   :entity-heiarchy [:item :character]
    :settings {:tile-dimensions [8 8]
-              :screen-dimensions [1 1]}))
+              :screen-dimensions [0.8 0.9]}))
 
-(defn game [] (rl/render @state))
+(defn game []
+  (let [game @state
+        [x y] (rl/game-dims game)
+        [r d] (rl/remaining-space game)]
+   [:div
+     [:div {:style {:position "absolute"}} (rl/render game)]
+     [:div {:style {:background-color "black"
+                    :color "white"
+                    :height (px (+ y d))
+                    :width (px r)
+                    :position "absolute"
+                    :left (px x)}}
+      [:div {:style {:text-align "center"}}
+       [:h1 "VIDEO GAME"]
+       [:button "Fight"]
+       [:button "Smooch"]]]
+    [:div {:style {:background-color "grey"
+                   :color "white"
+                   :position "absolute"
+                   :top (px y)
+                   :height (px d)
+                   :width (px y)}}
+     [:p "> The monster wants to fight!"]
+     [:p "> But maybe smooch too, wow"]]]))
 
 (rl/wander game state (. js/document (getElementById "app")))
 
-(defn on-js-reload [])
+(defn on-js-reload []) 
--- src/malandragem/core.cljs
+++ src/malandragem/core.cljs
@@ -3,10 +3,10 @@
             [reagent.core :as reagent])
   (:require-macros [cljs.core.async.macros :refer [go go-loop]]))
 
-(def default-container
-  {:style
-   {:padding "0"
-    :margin "0"}})
+(def default-container-style
+  {:padding "0"
+   :margin "0"
+   :position "relative"})
 
 (def default-data
   {:levels {}
@@ -14,12 +14,7 @@
 
 (def default-tile-style
   {:background-color "#FFFFFF"
-   :position "fixed"})
-
-(def default-tile-size-style
-  {:padding "0"
-   :margin "0"
-   :display "block"})
+   :position "absolute"})
 
 (defn get-level [game]
   (get (:levels game) (:level (:state game))))
@@ -31,6 +26,7 @@
 (defn transform-tile [default-style [t properties] & other-styles]
   (let [styles (into [default-style (:style properties)]
                      other-styles)]
+    (println [:div (assoc properties :style (apply merge styles))])
     [:div (assoc properties :style (apply merge styles))]))
 
 (defn tile-location-style [tile-size [x y]]
@@ -40,7 +36,7 @@
 (defn draw-tile [game coord]
   (let [{:keys [tiles]} (get-level game)
         tile-size-style (-> game ::impl ::tile-size-style)
-        tile-size ( ::size tile-size-style)
+        tile-size (-> game ::impl ::size)
         draw-fn (get tiles coord (:default tiles))
         tile-location (tile-location-style tile-size coord)]
     (transform-tile default-tile-style
@@ -48,14 +44,18 @@
                     tile-size-style
                     tile-location)))
 
-(defn draw-entity [game level entity])
+(defn draw-entity [game level entity]
+  )
+
+(defn draw-entities [game level]
+  [:div])
 
 (defn get-coords [x y]
   (for [x (range x)
         y (range y)]
     [x y]))
 
-(defn get-tile-size-style [settings]
+(defn resize-state [settings]
   (let [xscrn window.innerWidth
         yscrn window.innerHeight
         [sx sy] (:screen-dimensions settings)
@@ -63,14 +63,25 @@
         xsize (js/Math.floor (/ (* sx xscrn) xdims))
         ysize (js/Math.floor (/ (* sy yscrn) ydims))
         tile-size* (if (< xsize ysize) xsize ysize)
-        tile-size (str tile-size* "px")]
-    (merge default-tile-size-style
-           {:width tile-size :height tile-size
-            :min-width tile-size :min-height tile-size
-            :max-width tile-size :max-height tile-size
-            ::size tile-size*})))
+        tile-size (str tile-size* "px")
+        sx (* tile-size* xdims)
+        sy (* tile-size* ydims)
+        right (- xscrn sx)
+        down (- yscrn sy)]
+    {::box-size [sx sy]
+     ::remaining-space [right down]
+     ::size tile-size*
+     ::tile-size-style
+     {:width tile-size :height tile-size
+      :min-width tile-size :min-height tile-size
+      :max-width tile-size :max-height tile-size}}))
 
-(defn build-default [game])
+(defn build-default [game]
+  (let [[sx sy] (-> game ::impl ::box-size)
+        box-size {:max-height sy :max-width sx
+                  :height sy :width sx
+                  :min-height sy :min-width sx}]
+    [:div {:style (merge default-container-style box-size)}]))
 
 (defn render [game]
   (let [settings (:settings game)
@@ -81,17 +92,14 @@
      (into
       (map (partial draw-tile game))
       (get-coords x y))
-     (into
-      (map (partial draw-entity game level))
-      (:entities level)))))
+     (into (draw-entities game level)))))
 
 (defn resize-event [game-atom]
   (fn []
     (swap!
      game-atom
      (fn [game]
-       (assoc-in game [::impl ::tile-size-style]
-                 (get-tile-size-style (:settings game)))))))
+       (assoc game ::impl (resize-state (:settings game)))))))
 
 (defn register-state! [game-atom body]
   (let [resize-fn (resize-event game-atom)]
@@ -102,3 +110,8 @@
   (register-state! game-atom body)
   (reagent/render-component [game-fn] body))
 
+(defn game-dims [state] (-> state ::impl ::box-size))
+
+(defn remaining-space [state] (-> state ::impl ::remaining-space))
+
+(defn px [s] (str s "px"))
