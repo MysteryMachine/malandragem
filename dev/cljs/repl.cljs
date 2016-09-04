7b71caf9a008bdbb41b563094142a4278f7d70ac
Titus <Titus>
2016-09-03 21:39:40 -0400

    geeeez windows

--- dev/cljs/repl.cljs
+++
@@ -1,5 +0,0 @@
-(ns ^:figwheel-always cljs.repl
-  (:require [game.core :as g]
-            [malandragem.core :as gl]))
-
-(js/alert "hi")
---
+++ dev/repl/user.cljs
@@ -0,0 +1,4 @@
+(ns repl.user
+  (:require [game.core :as g]
+            [malandragem.core :as rl]))
+
